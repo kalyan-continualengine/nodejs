cf004d22f51d2d37ed1bff52ead09e2f8223542b
kalyan-continualengine <kalyan.kumar@continualengine.com>
2021-05-01 22:40:31 +0530

    login and signup apis

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+//IMPORT ROUTES
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -11,8 +13,9 @@ connection.once('open', function () {
     console.log('MongoDB server connection established successfully!')
 })
 
-const authRoute = require('./routes/auth');
-
+//MIDDLEWARE
+app.use(express.json());
+//ROUTER MIDDLEWARE
 app.use('/api/user', authRoute);
 
 app.listen(3000, () => console.log('Server is running in port 3000'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,52 @@
 const router = require('express').Router();
+const User = require('../model/User');
+const jwt = require('jsonwebtoken');
+const { registerValidation, loginValidation } = require('../validation');
 
-router.post('/register', (req, res) => {
-    res.send('register');
+//FOR SIGNUP API
+router.post('/signup', async (req, res) => {
+
+    //validating before adding to db
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //checking if user is already present in database
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send('User is already exist!!');
+
+    //create new user with postman data and save to db
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const saveUser = await user.save();
+        res.send(saveUser);
+    } catch (err) {
+        res.status(400).send('Something went wrong!!')
+    }
+});
+
+//FOR LOGIN API
+router.post('/login', async (req, res) => {
+    //validating before adding to db
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //checking the user email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Wrong Email!!');
+
+    //checking the user password
+    const passwordCheck = await User.findOne({ password: req.body.password });
+    if (!passwordCheck) return res.status(400).send('Wrong password!!');
+
+    //create and assigning token
+    const token = jwt.sign({ _id: user._id, name: user.name }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+
+    res.send('Successfully loggedin');
 })
-
-router.post('/login');
 
 module.exports = router;
