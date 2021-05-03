db0056b9184964bb3cf6c7db39d7fdbf1dae6672
kalyan-continualengine <kalyan.kumar@continualengine.com>
2021-05-03 15:24:03 +0530

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -43,7 +43,7 @@ router.post('/login', async (req, res) => {
     if (!passwordCheck) return res.status(400).send('Wrong password!!');
 
     //create and assigning token
-    const token = jwt.sign({ _id: user._id, name: user.name }, process.env.TOKEN_SECRET);
+    const token = jwt.sign({ _id: user._id, name: user.name, email: user.email }, process.env.TOKEN_SECRET);
     res.header('auth-token', token).send(token);
 
     res.send('Successfully loggedin');
